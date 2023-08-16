2a87b3d7fe40e020e8558c6dadcf6447963ae181
Jacob Khong <jacobkhongdev@gmail.com>
2023-08-16 21:41:32 +0800

    update rating data type to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_16_122717) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_134015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_16_122717) do
     t.string "title"
     t.text "overview"
     t.text "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
